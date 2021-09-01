c367b21809cea16639e4cd197e5e89d76b487b22
William Guilherme <wguilherme@securitygeek.io>
2021-09-01 14:23:23 -0700

    Initial Commit

---
+++ zpa-examples/zpa_backoff_test/main.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+        zpa = {
+            version = "1.0.0"
+            source = "zscaler.com/zpa/zpa"
+        }
+    }
+}
+
+provider "zpa" {}
+
+
+ resource "zpa_segment_group" "sg_all_other_services" {
+   count = "10"
+   name = "Terraform-${count.index + 1}"
+   description = "All Other Services"
+   enabled = true
+   policy_migrated = true
+ }
