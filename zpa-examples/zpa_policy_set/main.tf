bd6df45bf36be0d8223e8ae6f04f8029375d55d3
William Guilherme <wguilherme@securitygeek.io>
2021-09-04 00:24:06 -0700

    pushed policyset example

--- zpa-examples/zpa_policy_set/main.tf
+++ zpa-examples/zpa_policy_set/main.tf
@@ -13,20 +13,33 @@ provider "zpa" {}
 data "zpa_policy_set_global" "all" {
 }
 
-output "all_policy_set_global" {
+output "all_zpa_policy_set_global" {
   value = data.zpa_policy_set_global.all
 }
 
-
-
-resource "zpa_policyset_rule" "example2" {
-
-  name                          = "example2"
-  description                   = "example2"
-  action                        = "ALLOW"
-  rule_order                     = 1
-  policy_set_id = data.zpa_policy_set_global.all.id
-  policy_type = 1
+data "zpa_application_segment" "all_other_services"{
+  id = 216196257331283285
 }
 
+data "zpa_idp_controller" "okta"{
+  id = 216196257331281956
+}
 
+resource "zpa_policyset_rule" "all_other_services" {
+  name                          = "All Other Services"
+  description                   = "All Other Services"
+  action                        = "ALLOW"
+  rule_order                     = 2
+  operator = "AND"
+  policy_set_id = data.zpa_policy_set_global.all.id
+  conditions {
+     negated = false
+     operator = "OR"
+    operands {
+      name =  "All Other Services"
+      object_type = "APP"
+      lhs = "id"
+      rhs = data.zpa_application_segment.all_other_services.id 
+    }
+  }
+}
