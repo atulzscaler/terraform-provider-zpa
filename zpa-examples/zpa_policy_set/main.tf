cb0d9b6a85d0b5e9569ffb626381839afa8d1a05
William Guilherme <wguilherme@securitygeek.io>
2021-09-04 22:01:40 -0700

    Initial Commit

--- zpa-examples/zpa_policy_set/main.tf
+++ zpa-examples/zpa_policy_set/main.tf
@@ -10,17 +10,6 @@ terraform {
 provider "zpa" {}
 
 
-data "zpa_policy_set_global" "all" {
-}
-
-// output "all_zpa_policy_set_global" {
-//   value = data.zpa_policy_set_global.all
-// }
-
-data "zpa_application_segment" "all_other_services"{
-  id = 216196257331283285
-}
-
 resource "zpa_policyset_rule" "all_other_services" {
   name                          = "All Other Services"
   description                   = "All Other Services"
@@ -29,29 +18,47 @@ resource "zpa_policyset_rule" "all_other_services" {
   operator = "AND"
   policy_set_id = data.zpa_policy_set_global.all.id
   conditions {
-     negated = false
-     operator = "OR"
+    negated = false
+    operator = "OR"
     operands {
       name =  "All Other Services"
       object_type = "APP"
       lhs = "id"
-      rhs = data.zpa_application_segment.all_other_services.id 
-    }
-    operands {
-      object_type = "POSTURE"
-      lhs = data.zpa_application_segment.all_other_services.id 
-      rhs = true
+      rhs = data.zpa_application_segment.all_other_services.id
     }
   }
+  // conditions {
+  //    negated = false
+  //    operator = "OR"
+    // operands {
+    //   object_type = "IDP"
+    //   lhs = "id"
+    //   rhs = data.zpa_idp_controller.sgio_user_okta.id
+    // }
+  //   operands {
+  //     object_type = "SCIM_GROUP"
+  //     lhs = data.zpa_idp_controller.sgio_user_okta.id
+  //     rhs = data.zpa_scim_groups.engineering.id
+  //     idp_id = data.zpa_idp_controller.sgio_user_okta.id
+  //   }
+  // }
 }
 
+output "all_zpa_policyset_rule" {
+  value = zpa_policyset_rule.all_other_services
+}
 
+data "zpa_policy_set_global" "all" {
+}
 
-// resource "zpa_policyset_rule" "example" {
-//   name                          = "example1"
-//   description                   = "example1"
-//   action                        = "ALLOW"
-//   ruleorder                     = 1
-//   policysetid = data.zpa_policy_set_global.all.id
-//   policytype = 1
-// }
+data "zpa_application_segment" "all_other_services"{
+  id = 216196257331283285
+}
+
+data "zpa_idp_controller" "sgio_user_okta" {
+ id = "216196257331281956"
+}
+
+data "zpa_scim_groups" "engineering" {
+ id = "255066"
+}
