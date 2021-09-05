a01709a09487026768f78e111de9f42074aeb69d
William Guilherme <wguilherme@securitygeek.io>
2021-09-05 14:26:01 -0700

    changed scim groups struct types to strings

--- zscaler/data_source_zpa_scim_group.go
+++ zscaler/data_source_zpa_scim_group.go
@@ -1,7 +1,7 @@
 package zscaler
 
 import (
-	"strconv"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -11,7 +11,7 @@ func dataSourceScimGroup() *schema.Resource {
 		Read: dataSourceScimGroupRead,
 		Schema: map[string]*schema.Schema{
 			"creation_time": {
-				Type:     schema.TypeInt,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"id": {
@@ -23,11 +23,11 @@ func dataSourceScimGroup() *schema.Resource {
 				Computed: true,
 			},
 			"idp_id": {
-				Type:     schema.TypeInt,
+				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"modified_time": {
-				Type:     schema.TypeInt,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"name": {
@@ -41,21 +41,15 @@ func dataSourceScimGroup() *schema.Resource {
 func dataSourceScimGroupRead(d *schema.ResourceData, m interface{}) error {
 	zClient := m.(*Client)
 
-	// id := d.Get("id").(string)
-	// log.Printf("[INFO] Getting data for scim group %s\n", id)
-
-	id, err := strconv.ParseInt(d.Get("id").(string), 10, 64)
-	if err != nil {
-		return err
-	}
+	id := d.Get("id").(string)
+	log.Printf("[INFO] Getting data for scim group %s\n", id)
 
 	resp, _, err := zClient.scimgroup.Get(id)
 	if err != nil {
 		return err
 	}
 
-	// d.SetId(resp.ID)
-	d.SetId(strconv.FormatInt(int64(resp.ID), 10))
+	d.SetId(resp.ID)
 	_ = d.Set("creation_time", resp.CreationTime)
 	_ = d.Set("idp_group_id", resp.IdpGroupId)
 	_ = d.Set("idp_id", resp.IdpId)
@@ -65,18 +59,3 @@ func dataSourceScimGroupRead(d *schema.ResourceData, m interface{}) error {
 	return nil
 
 }
-
-// func flattenScimGroups(scimGroupResponse []scimgroup.ScimGroup) []interface{} {
-// 	scimGroups := make([]interface{}, len(scimGroupResponse))
-// 	for i, scimGroupItem := range scimGroupResponse {
-// 		scimGroups[i] = map[string]interface{}{
-// 			"creation_time": scimGroupItem.CreationTime,
-// 			"id":            scimGroupItem.ID,
-// 			"idp_id":        scimGroupItem.IdpId,
-// 			"idp_group_id":  scimGroupItem.IdpGroupId,
-// 			"modified_time": scimGroupItem.ModifiedTime,
-// 			"name":          scimGroupItem.Name,
-// 		}
-// 	}
-// 	return scimGroups
-// }
