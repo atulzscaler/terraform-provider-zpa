6c06973c4479a73b9595438c7854c7d34b7e6157
William Guilherme <wguilherme@securitygeek.io>
2021-09-04 22:52:22 -0700

    added main.tf

--- zpa-examples/zpa_policy_set/main.tf
+++ zpa-examples/zpa_policy_set/main.tf
@@ -9,17 +9,24 @@ terraform {
 
 provider "zpa" {}
 
+// data "zpa_policy_set_global" "all" {
+// }
+
+// output "all_zpa_policyset_rule" {
+//   value = data.zpa_policy_set_global.all
+// }
+
 
 resource "zpa_policyset_rule" "all_other_services" {
   name                          = "All Other Services"
   description                   = "All Other Services"
   action                        = "ALLOW"
   rule_order                     = 2
-  operator = ["AND"]
+  operator = "AND"
   policy_set_id = data.zpa_policy_set_global.all.id
   conditions {
     negated = false
-    operator = ["OR"]
+    operator = "OR"
     operands {
       name =  "All Other Services"
       object_type = "APP"
@@ -27,21 +34,21 @@ resource "zpa_policyset_rule" "all_other_services" {
       rhs = data.zpa_application_segment.all_other_services.id
     }
   }
-  conditions {
-     negated = false
-     operator = ["OR"]
-    // operands {
-    //   object_type = "IDP"
-    //   lhs = "id"
-    //   rhs = data.zpa_idp_controller.sgio_user_okta.id
-    // }
-    operands {
-      object_type = "SCIM_GROUP"
-      lhs = data.zpa_idp_controller.sgio_user_okta.id
-      rhs = data.zpa_scim_groups.engineering.id
-      idp_id = data.zpa_idp_controller.sgio_user_okta.id
-    }
-  }
+  // conditions {
+  //    negated = false
+  //    operator = "OR"
+  //   operands {
+  //     object_type = "IDP"
+  //     lhs = "id"
+  //     rhs = data.zpa_idp_controller.sgio_user_okta.id
+  //   }
+  //   operands {
+  //     object_type = "SCIM_GROUP"
+  //     lhs = data.zpa_idp_controller.sgio_user_okta.id
+  //     rhs = data.zpa_scim_groups.engineering.id
+  //     idp_id = data.zpa_idp_controller.sgio_user_okta.id
+  //   }
+  // }
 }
 
 output "all_zpa_policyset_rule" {
