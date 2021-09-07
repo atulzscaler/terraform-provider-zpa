6076947a55dce2164de68e4804c051ba64709034
William Guilherme <wguilherme@securitygeek.io>
2021-09-07 13:24:43 -0700

    Corrected server_group field from app_server_group

--- zpa-examples/zpa_browser_access/main.tf
+++ zpa-examples/zpa_browser_access/main.tf
@@ -9,13 +9,27 @@ terraform {
 
 provider "zpa" {}
 
-resource "zpa_server_group" "example" {
-  name = "example"
-  description = "example"
+data "zpa_app_connector_group" "example" {
+  name = "SGIO-Vancouver"
+}
+
+resource "zpa_server_group" "example1" {
+  name = "example1"
+  description = "example1"
   enabled = true
   dynamic_discovery = true
   app_connector_groups {
-    id = 216196257331281931
+    id = [data.zpa_app_connector_group.example.id]
+  }
+}
+
+resource "zpa_server_group" "example2" {
+  name = "example2"
+  description = "example2"
+  enabled = true
+  dynamic_discovery = true
+  app_connector_groups {
+    id = [data.zpa_app_connector_group.example.id]
   }
 }
 
@@ -24,10 +38,6 @@ resource "zpa_server_group" "example" {
    description = "example"
    enabled = true
    policy_migrated = true
-    // applications  {
-    //     //name = [data.zpa_application_segment.application_segment.name]
-    //     id = 216196257331282544
-    // }
  }
 
 // DevOps Browser Access
@@ -43,27 +53,8 @@ resource "zpa_browser_access" "jenkins_app" {
     health_reporting = "ON_ACCESS"
     bypass_type = "NEVER"
     tcp_port_ranges = ["80", "80", "8080", "8080"]
-    domain_names = ["sales.securitygeek.io", "qa.securitygeek.io", "jenkins.securitygeek.io"]
-    segment_group_id = zpa_segment_group.sg_sgio_browser_access.id
-
-    clientless_apps {
-        name = "sales.securitygeek.io"
-        application_protocol = "HTTP"
-        application_port = "80"
-        certificate_id = data.zpa_ba_certificate.sales_ba.id
-        trust_untrusted_cert = true
-        enabled = true
-        domain = "sales.securitygeek.io"
-    }
-        clientless_apps {
-        name = "qa.securitygeek.io"
-        application_protocol = "HTTP"
-        application_port = "80"
-        certificate_id = data.zpa_ba_certificate.qa_ba.id
-        trust_untrusted_cert = true
-        enabled = true
-        domain = "qa.securitygeek.io"
-    }
+    domain_names = ["jenkins.securitygeek.io"]
+    segment_group_id = zpa_segment_group.example.id
 
     clientless_apps {
         name = "jenkins.securitygeek.io"
@@ -74,7 +65,10 @@ resource "zpa_browser_access" "jenkins_app" {
         enabled = true
         domain = "jenkins.securitygeek.io"
     }
-    app_server_groups {
-        id = [zpa_server_group.example.id]
+    server_groups {
+        id = [
+            zpa_server_group.example1.id,
+            zpa_server_group.example2.id
+        ]
     }
 }
