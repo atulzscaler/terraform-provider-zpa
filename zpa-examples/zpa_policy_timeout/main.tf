c457f7858b1a56040f3ce49a0bf4173e97286310
William Guilherme <wguilherme@securitygeek.io>
2021-09-07 17:02:52 -0700

    Updated policy timeout main.tf example

--- zpa-examples/zpa_policy_timeout/main.tf
+++ zpa-examples/zpa_policy_timeout/main.tf
@@ -9,6 +9,36 @@ terraform {
 
 provider "zpa" {}
 
+resource "zpa_application_segment" "all_other_services" {
+    name = "All Other Services"
+    description = "All Other Services"
+    enabled = true
+    health_reporting = "ON_ACCESS"
+    bypass_type = "NEVER"
+    tcp_port_ranges = ["1", "52", "54", "65535"]
+    domain_names = ["*.securitygeek.io"]
+    segment_group_id = zpa_segment_group.sg_all_other_services.id
+    server_groups {
+        id = [zpa_server_group.all_other_services.id]
+    }
+}
+
+ resource "zpa_segment_group" "sg_all_other_services" {
+   name = "All Other Services"
+   description = "All Other Services"
+   enabled = true
+   policy_migrated = true
+ }
+
+ resource "zpa_server_group" "all_other_services" {
+  name = "All Other Services"
+  description = "All Other Services"
+  enabled = true
+  dynamic_discovery = true
+  app_connector_groups {
+    id = [data.zpa_app_connector_group.sgio-vancouver.id]
+  }
+}
 
 resource "zpa_policy_timeout" "all_other_services" {
   name                          = "All Other Services"
@@ -25,9 +55,9 @@ resource "zpa_policy_timeout" "all_other_services" {
     operator = "OR"
     operands {
       name =  "All Other Services"
-      object_type = "APP"
+      object_type = "APP_GROUP"
       lhs = "id"
-      rhs = data.zpa_application_segment.all_other_services.id
+      rhs = zpa_segment_group.sg_all_other_services.id
     }
   }
   conditions {
@@ -39,6 +69,7 @@ resource "zpa_policy_timeout" "all_other_services" {
       rhs = data.zpa_scim_groups.engineering.id
       idp_id = data.zpa_idp_controller.sgio_user_okta.id
     }
+  }
 }
 
 output "all_zpa_policy_timeout" {
@@ -46,4 +77,17 @@ output "all_zpa_policy_timeout" {
 }
 
 data "zpa_policy_timeout" "all" {
+}
+
+data "zpa_app_connector_group" "sgio-vancouver" {
+  name = "SGIO-Vancouver"
+}
+
+data "zpa_idp_controller" "sgio_user_okta" {
+ name = "SGIO-User-Okta"
+}
+
+data "zpa_scim_groups" "engineering" {
+  name = "Engineering"
+  idp_name = "SGIO-User-Okta"
 }
