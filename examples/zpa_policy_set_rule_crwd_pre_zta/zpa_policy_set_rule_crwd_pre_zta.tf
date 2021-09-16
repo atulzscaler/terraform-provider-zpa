d91d72053120ce5dd8728ca81a99a5a0c8015887
William Guilherme <wguilherme@securitygeek.io>
2021-09-16 15:25:53 -0700

    Added policyset_rule_example

--- examples/zpa_policy_set_rule_crwd_pre_zta/zpa_policy_set_rule_crwd_pre_zta.tf
+++ examples/zpa_policy_set_rule_crwd_pre_zta/zpa_policy_set_rule_crwd_pre_zta.tf
@@ -1,15 +1,37 @@
-resource "zpa_policyset_rule" "crwd_zpa_pre_zta" {
-  name                          = "CrowdStrike_ZPA_Pre-ZTA"
-  description                   = "CrowdStrike_ZPA_Pre-ZTA"
+terraform {
+    required_providers {
+        zpa = {
+            version = "1.0.0"
+            source = "zscaler.com/zpa/zpa"
+        }
+    }
+}
+
+provider "zpa" {}
+
+// CrowdStrike_ZTA_Score_Policy
+resource "zpa_policyset_rule" "crwd_zta_score_40" {
+  name                          = "CrowdStrike_ZTA_Score_40"
+  description                   = "CrowdStrike_ZTA_Score_40"
   action                        = "DENY"
+  rule_order                    = 1
   operator = "AND"
   policy_set_id = data.zpa_policy_set_global.all.id
   conditions {
     negated = false
     operator = "OR"
     operands {
+      object_type = "APP"
+      lhs = "id"
+      rhs = zpa_application_segment.as_intranet_web_apps.id
+    }
+  }
+  conditions {
+    negated = false
+    operator = "OR"
+    operands {
       object_type = "POSTURE"
-      lhs = data.zpa_posture_profile.crwd_zpa_pre_zta.posture_udid
+      lhs = data.zpa_posture_profile.crwd_zta_score_40.posture_udid
       rhs = false
     }
   }
@@ -18,23 +40,70 @@ resource "zpa_policyset_rule" "crwd_zpa_pre_zta" {
      operator = "OR"
     operands {
       object_type = "SAML"
-      lhs = data.zpa_saml_attribute.email_user_sso.id
-      rhs = "user1@example.com"
+      lhs = data.zpa_saml_attribute.email_sgio_user_sso.id
+      rhs = "alison.abbas@securitygeek.io"
       idp_id = data.zpa_idp_controller.sgio_user_okta.id
     }
   }
 }
 
-data "zpa_policy_set_global" "example" {}
-
-data "zpa_idp_controller" "idp_example" {
- name = "idp_example"
+resource "zpa_application_segment" "as_intranet_web_apps" {
+    name = "SGIO Intranet Web Apps"
+    description = "SGIO Intranet Web Apps"
+    enabled = true
+    health_reporting = "ON_ACCESS"
+    bypass_type = "NEVER"
+    tcp_port_ranges = ["80", "80"]
+    domain_names = ["intranet.securitygeek.io", "qa.securitygeek.io"]
+    segment_group_id = zpa_segment_group.sg_sgio_intranet_web_apps.id
+    server_groups {
+        id = [zpa_server_group.sgio_intranet_web_apps.id]
+    }
 }
 
-data "zpa_saml_attribute" "email_user_sso" {
-    name = "Email_User SSO"
+resource "zpa_server_group" "sgio_intranet_web_apps" {
+  name = "SGIO Intranet Web Apps"
+  description = "SGIO Intranet Web Apps"
+  enabled = true
+  dynamic_discovery = false
+  servers {
+    id = [
+      zpa_application_server.intranet.id
+    ]
+  }
+  app_connector_groups {
+    id = [data.zpa_app_connector_group.sgio-vancouver.id]
+  }
 }
 
-data "zpa_posture_profile" "crwd_zpa_pre_zta" {
- name = "CrowdStrike_ZPA_Pre-ZTA"
+   resource "zpa_segment_group" "sg_sgio_intranet_web_apps" {
+   name = "SGIO Intranet Web Apps"
+   description = "SGIO Intranet Web Apps"
+   enabled = true
+   policy_migrated = true
+ }
+
+ resource "zpa_application_server" "intranet" {
+  name                          = "intranet.securitygeek.io"
+  description                   = "intranet.securitygeek.io"
+  address                       = "intranet.securitygeek.io"
+  enabled                       = true
+}
+
+data "zpa_policy_set_global" "all" {}
+
+data "zpa_idp_controller" "sgio_user_okta" {
+ name = "SGIO-User-Okta"
+}
+
+data "zpa_saml_attribute" "email_sgio_user_sso" {
+    name = "Email_SGIO-User-Okta"
+}
+
+data "zpa_posture_profile" "crwd_zta_score_40" {
+ name = "CrowdStrike_ZPA_ZTA_40"
+}
+
+data "zpa_app_connector_group" "sgio-vancouver" {
+  name = "SGIO-Vancouver"
 }
