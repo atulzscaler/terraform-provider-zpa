ac9a9967baa9dd1c08605b1a68ccf18d2060b1d3
William Guilherme <wguilherme@securitygeek.io>
2021-09-21 22:05:52 -0700

    Updated zpa_policy_forwarding example

--- examples/zpa_policy_forwarding/main.tf
+++ examples/zpa_policy_forwarding/main.tf
@@ -1,47 +1,18 @@
-terraform {
-    required_providers {
-        zpa = {
-            version = "1.0.0"
-            source = "zscaler.com/zpa/zpa"
-        }
-    }
-}
-
-provider "zpa" {}
-
-data "zpa_policy_forwarding" "all" {
-}
-
-data "zpa_idp_controller" "sgio_user_okta" {
- name = "SGIO-User-Okta"
-}
-
-// Okta IDP SCIM Groups
-data "zpa_scim_groups" "engineering" {
-  name = "Engineering"
-  idp_name = "SGIO-User-Okta"
-}
-
-// Application Connector Groups
-data "zpa_app_connector_group" "sgio-vancouver" {
-  name = "SGIO-Vancouver"
-}
-
-resource "zpa_policy_forwarding" "example" {
+// Create Policy Forwarding Rule
+resource "zpa_policy_forwarding_rule" "" {
   name                          = "example"
   description                   = "example"
   action                        = "INTERCEPT_ACCESSIBLE"
   operator = "AND"
-  policy_set_id = data.zpa_policy_forwarding.all.id
+  policy_set_id = data.zpa_global_policy_forwarding.policyset.id
 
   conditions {
     negated = false
     operator = "OR"
     operands {
-      name =  "SGIO DevOps Servers"
       object_type = "APP"
       lhs = "id"
-      rhs = zpa_application_segment.as_sgio_devops.id
+      rhs = [zpa_application_segment.crm_application.id]
     }
   }
   conditions {
@@ -49,52 +20,72 @@ resource "zpa_policy_forwarding" "example" {
      operator = "OR"
     operands {
       object_type = "SCIM_GROUP"
-      lhs = data.zpa_idp_controller.sgio_user_okta.id
-      rhs = data.zpa_scim_groups.engineering.id
-      idp_id = data.zpa_idp_controller.sgio_user_okta.id
+      lhs = data.zpa_idp_controller.idp_name.id
+      rhs = [data.zpa_scim_groups.engineering.id]
     }
   }
 }
 
-resource "zpa_application_segment" "as_sgio_devops" {
-    name = "SGIO DevOps Servers"
-    description = "SGIO DevOps Servers"
+// Create Application Segment
+resource "zpa_application_segment" "crm_application" {
+    name = "CRM Application"
+    description = "CRM Application"
     enabled = true
     health_reporting = "ON_ACCESS"
     bypass_type = "NEVER"
-    tcp_port_ranges = ["8080", "8080"]
-    domain_names = ["jenkins.securitygeek.io"]
-    segment_group_id = zpa_segment_group.sg_sgio_devops.id
+    is_cname_enabled = true
+    tcp_port_ranges = ["80", "80"]
+    domain_names = ["crm.example.com"]
+    segment_group_id = zpa_segment_group.crm_app_group.id
     server_groups {
-        id = [zpa_server_group.sgio_devops_servers.id]
+        id = [ zpa_server_group.crm_servers.id ]
     }
 }
 
-  resource "zpa_segment_group" "sg_sgio_devops" {
-   name = "SGIO DevOps Servers"
-   description = "SGIO DevOps Servers"
-   enabled = true
-   policy_migrated = true
- }
-
- resource "zpa_server_group" "sgio_devops_servers" {
-  name = "SGIO DevOps Servers"
-  description = "SGIO DevOps Servers"
+// Create Server Group
+resource "zpa_server_group" "crm_servers" {
+  name = "CRM Servers"
+  description = "CRM Servers"
   enabled = true
   dynamic_discovery = false
-    servers {
-    id = [
-      zpa_application_server.jenkins.id,
-    ]
-  }
   app_connector_groups {
-    id = [data.zpa_app_connector_group.sgio-vancouver.id]
+    id = [ data.zpa_app_connector_group.dc_connector_group.id ]
+  }
+  servers {
+    id = [ zpa_application_server.crm_app_server.id ]
   }
 }
 
-resource "zpa_application_server" "jenkins" {
-  name                          = "jenkins.securitygeek.io"
-  description                   = "jenkins.securitygeek.io"
-  address                       = "jenkins.securitygeek.io"
+// Create Application Server
+resource "zpa_application_server" "crm_app_server" {
+  name                          = "CRM App Server"
+  description                   = "CRM App Server"
+  address                       = "crm.example.com"
   enabled                       = true
+}
+
+// Create Segment Group
+resource "zpa_segment_group" "crm_app_group" {
+  name = "CRM App group"
+  description = "CRM App group"
+  enabled = true
+  policy_migrated = true
+}
+
+// Retrieve App Connector Group
+data "zpa_app_connector_group" "dc_connector_group" {
+  name = "DC Connector Group"
+}
+
+data "zpa_global_policy_forwarding" "policyset" {
+}
+
+data "zpa_idp_controller" "idp_name" {
+ name = "IdP-Name"
+}
+
+// Okta IDP SCIM Groups
+data "zpa_scim_groups" "engineering" {
+  name = "Engineering"
+  idp_name = "IdP-Name"
 }
